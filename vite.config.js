e7ec186386b67c937e1857446373e63e2f2a9795
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-03-13 20:43:19 -0300

    updated ssl

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import fs from 'node:fs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iris_frontend/"
+  base: "/iris_frontend/",
+  server: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/34.44.177.109.nip.io/privkey.perm'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/34.44.177.109.nip.io/cert.pem')
+    }
+  },
+  build: {
+    outDir: 'build'
+  }
 })
